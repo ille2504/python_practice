5079b43ed25456e89160adf4175e96183fbaa201
Mario Illescas <mario.ille25@gmail.com>
2024-02-13 14:13:43 -0600

    string file information

--- string_1.py
+++ string_1.py
@@ -1,6 +1,56 @@
-print("hello world")
+mystring = "mystring"
 
-list = [1, 3, 3, 5]
-res = set(list)
+my_string = '''
+hello\
+my\
+name\
+is\
+string'''
 
-print(res)
+string_1 = 'cisco router'
+
+print(string_1[6])
+
+print(len(string_1))
+
+
+a = "Cisco Router"
+
+print(a.find('o'))
+
+b = "   mario "
+
+print(b.strip())
+
+c = " ma r i o"
+print(c.replace(" ", ""))
+
+d = 'Juniper,Cisco,HP,Avaya,Goto'
+
+print(d.split(","))
+
+print('_'.join(a))
+
+m = 'cisco'
+n = 'router'
+
+print(m + n)
+
+print("o" in m)
+
+x = 'my name is %s my age is %d my weight is %.1f' % ('mario', 35, 160.5)
+print(x)
+
+y = 'my name is {} my age is {} my weight is {}'.format('mario', 35, 160.5)
+print(y)
+
+
+model = '2004MX'
+slot = 4
+ios = 12.4
+
+print(f'cisco model: {model}, with {slot} slots, and the ios is {ios}')
+
+slice = 'se/pythontutorial/learn/lecture/11804310#overview'
+
+print(slice[3:17])
